442bfc52f93976ad90d69a1ae71a2c23de987463
Daniel Soffner <dsoffner@redhat.com>
2017-02-17 11:52:10 +1100

    Changed code

--- app.js
+++ app.js
@@ -1,47 +1,69 @@
-var express = require('express');
-var mysql = require('mysql');
 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+//load customers route
+var customers = require('./routes/customers'); 
 var app = express();
 
-//app configuration
-var ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var connection  = require('express-myconnection'); 
+var mysql = require('mysql');
 
-//mysql configuration
-var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
-var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
-var mysqlUser = 'dbadmin'; //mysql username
-var mysqlPass = 'dbpassword'; //mysql password
-var mysqlDb   = 'nodejs'; //mysql database name
+// all environments
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
 
-//connection strings
-var mysqlString = 'mysql://'   + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
+app.use(express.static(path.join(__dirname, 'public')));
 
-//connect to mysql
-var mysqlClient = mysql.createConnection(mysqlString);
-mysqlClient.connect(function(err){
-  if (err) console.log(err);
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+/*------------------------------------------
+    connection peer, register as middleware
+    type koneksi : single,pool and request 
+-------------------------------------------*/
+
+app.use(
+    
+    connection(mysql,{
+        
+        host: '127.0.0.1',
+        user: 'dbadmin',
+        password : 'dbpassword',
+        port : 3306, //port mysql
+        database:'nodejs'
+
+    },'pool') // pool or single
+
+);
+
+
+
+app.get('/', routes.index);
+app.get('/customers', customers.list);
+app.get('/customers/add', customers.add);
+app.post('/customers/add', customers.save);
+app.get('/customers/delete/:id', customers.delete_customer);
+app.get('/customers/edit/:id', customers.edit);
+app.post('/customers/edit/:id',customers.save_edit);
+
+
+app.use(app.router);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-// app is running!
-app.get('/', function(req, res) {
-  res.send('OK');
-});
-
-//MySQL is running!
-app.get('/mysql', function(req, res) {
-  mysqlClient.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) {
-      res.send('NOT OK' + JSON.stringify(err));
-    } else {
-      res.send('OK: ' + rows[0].solution);
-    }
-  });
-});
-
-
-app.listen(port, ipaddr);
-
-console.log('Server running at http://' + ipaddr + ':' + port + '/');
-console.log('MySQL running at mysql://[user:password]@' + mysqlHost + ':' + mysqlPort + '/nodejs');
