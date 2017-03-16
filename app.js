5a7da388abfb31e38919df2591f17381737cc79c
Daniel Soffner <dsoffner@redhat.com>
2017-03-16 15:02:36 +1100

    Update

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(
 
     connection(mysql,{
 
-        host: '172.30.80.17', // host mysql service
+        host: '172.30.15.160', // host mysql service
         user: 'dbadmin',
         password : 'dbpassword',
         port : 3306, // port mysql service
